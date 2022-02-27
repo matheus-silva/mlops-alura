1869928d28049a89d54b4c3399bf0fb2e7f4d314
Matheus da Silva Ferreira <matheus.dasilva.ferreira@hotmail.com>
2022-02-27 14:19:02 -0300

    feat: added an endpoint for the house prediction model

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from flask import Flask
 from textblob import TextBlob
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+df = pd.read_csv("data/casas.csv")
+colunas = ["tamanho", "preco"]
+df = df[colunas]
+X = df.drop("preco", axis="columns")
+y = df["preco"]
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42
+)
+modelo = LinearRegression()
+modelo.fit(X_train, y_train)
 
 app = Flask(__name__)
 
@@ -13,6 +27,11 @@ def sentimento(frase):
     tb_en = tb.translate(to="en")
     polaridade = tb_en.sentiment.polarity
     return f"polaridade: {polaridade}"
+
+@app.route("/cotacao/<int:tamanho>")
+def cotacao(tamanho):
+    preco = modelo.predict([[tamanho]])
+    return str(preco)
 
 if __name__ == "__main__":
     app.run(debug=True)
